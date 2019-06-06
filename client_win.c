460f79ccac9af19bf528ec6e653a7ca8255cf860
ljw8161 <45479186+ljw8161@users.noreply.github.com>
2019-06-06 20:46:19 +0900

    Add files via upload

--- client_win.c
+++ client_win.c
@@ -1,30 +1,33 @@
-#pragma comment(lib, "ws2_32.lib");
+#pragma comment(lib, "ws2_32.lib")
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
 #include <windows.h>
+#include <Windows.h>
 #include <process.h>
 #include <time.h> 
 #define BUF_SIZE 100
 #define NORMAL_SIZE 20
 #define LINES 200
 
+typedef struct { // 산성비(단어) 구조체
+	int x; // 단어의 x좌표
+	char word[20]; // 단어 저장 공간
+} rain;
+
 unsigned WINAPI send_msg(void* arg);
 unsigned WINAPI recv_msg(void* arg);
 void move(int, int);
 void error_handling(char* msg);
+void prnscreen();
+rain* recv_rain(void* arg);
 
 
 char name[NORMAL_SIZE];     // name
 char msg_form[NORMAL_SIZE];            // msg form
 char serv_time[NORMAL_SIZE];        // server time
 char msg[BUF_SIZE];                    // msg
-
-typedef struct { // 산성비(단어) 구조체
-	int x; // 단어의 x좌표
-	char word[20]; // 단어 저장 공간
-} rain;
 
 int line = 21;// 20번째 rains는 판정선.(0~19번째까지만 화면 표시)
 rain *rains;
@@ -49,7 +52,7 @@ int main(int argc, char *argv[])
 		t->tm_min);
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		error_Handling("WSAStartup() error!");
+		error_handling("WSAStartup() error!");
 
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serv_addr, 0, sizeof(serv_addr));
@@ -71,29 +74,27 @@ int main(int argc, char *argv[])
 
 
 	closesocket(sock);
-	endwin();
+//	endwin();
 	return 0;
 }
 
 unsigned WINAPI send_msg(void* arg)
 {
-	SOCKET sock = *((SOCKET*)arg);
+	int sock = *((int*)arg);
 	char name_msg[NORMAL_SIZE + BUF_SIZE];
 	char myInfo[BUF_SIZE];
 	char* who = NULL;
 	char temp[BUF_SIZE];
 	/** send join messge **/
 	//sprintf(myInfo, "%s's join. IP_%s\n",name , clnt_ip);
-	send(sock, myInfo, strlen(myInfo), 0);
+	send(sock, name, strlen(name), 0);
 
 	//system("Acid Raining");
 	system("mode con:cols=200 lines=200 | title 산성비");
 
 	while (1) {
-		move(LINES - 50, 0);
-		printf("--------------------------------------------------------------------");
 		move(LINES - 49, 0);
-		printf("q or Q to quit: ");
+		printf("q or Q to quit: "); // 사용자의 입력부
 		scanf("%s", msg);
 		printf(msg);
 		//fgets(msg, BUF_SIZE, stdin);
@@ -118,16 +119,15 @@ unsigned WINAPI recv_msg(void* arg)
 	int str_len;
 
 	while (1) {
-		str_len = recv(sock, name_msg, NORMAL_SIZE + BUF_SIZE - 1, 0);
-		if (str_len == -1)
-			return (void*)-1;
+		rains = recv_rain(arg);
+		prnscreen();
 	}
 	return NULL;
 }
 
 void move(int x, int y)
 {
-	HANDLE consoleHandle = GetStdHAndle(STD_OUTPUT_HANDLE);
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD pos;
 	pos.X = x;
 	pos.Y = y;
@@ -141,15 +141,16 @@ void error_handling(char* msg)
 	exit(1);
 }
 
-rain recv_rain(SOCKET sock)
+rain* recv_rain(void* arg)
 {
+	int sock = *((int*)arg);
 	int retval;
 	char suBuffer[21];
 
 	retval = recv(sock, suBuffer, line, 0);
 	if (retval == SOCKET_ERROR)
 	{
-		err_display("recv() error!");
+		error_handling("recv() error!");
 	}
 
 	rain *rains_temp;
@@ -157,7 +158,7 @@ rain recv_rain(SOCKET sock)
 	suBuffer[retval] = '\0';
 	rains_temp = (rain*)suBuffer;
 
-	return *rains_temp;
+	return rains_temp;
 }
 
 void prnscreen() // 화면 출력 함수
@@ -172,5 +173,5 @@ void prnscreen() // 화면 출력 함수
 	if (strcmp(rains[20].word, " ")) // 판정선에 단어가 남아있으면
 		ph -= 0.5; // 0.5씩 산성화 시킴
 	printf("\n[ pH ] %.1lf  [ 시간 ] %.2lf초\n\n", ph, sec); // 수소 이온 농도와 총 시간 출력
-	printf("입력>"); // 사용자의 입력부
+	
 }
